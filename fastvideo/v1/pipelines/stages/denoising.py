39dec8f333014826859d0b05990cd00d2b4d885b
Peiyuan Zhang <a1286225768@gmail.com>
2025-04-08 01:49:51 +0000

    done

--- fastvideo/v1/pipelines/stages/denoising.py
+++ fastvideo/v1/pipelines/stages/denoising.py
@@ -21,7 +21,7 @@ from fastvideo.v1.logger import init_logger
 from fastvideo.v1.pipelines.pipeline_batch_info import ForwardBatch
 from fastvideo.v1.pipelines.stages.base import PipelineStage
 from fastvideo.v1.utils import PRECISION_TO_TYPE
-
+from fastvideo.v1.platforms import _Backend
 logger = init_logger(__name__)
 
 
@@ -136,12 +136,11 @@ class DenoisingStage(PipelineStage):
                     self.attn_backend = get_attn_backend(
                         head_size=attn_head_size,
                         dtype=torch.float16,  # TODO(will): hack
-                        supported_attention_backends=["SLIDING_TILE_ATTN", "FLASH_ATTN", "TORCH_SDPA"] # hack
+                        supported_attention_backends=[_Backend.SLIDING_TILE_ATTN, _Backend.FLASH_ATTN, _Backend.TORCH_SDPA] # hack
                     )
                     from fastvideo.v1.attention.backends.sliding_tile_attn import (
                         SlidingTileAttentionBackend)
-                    if isinstance(self.attn_backend,
-                                  SlidingTileAttentionBackend):
+                    if self.attn_backend ==  SlidingTileAttentionBackend:
                         self.attn_metadata_builder_cls = self.attn_backend.get_builder_cls(
                         )
                         if self.attn_metadata_builder_cls is not None:
@@ -158,7 +157,6 @@ class DenoisingStage(PipelineStage):
                             attn_metadata = None
                     else:
                         attn_metadata = None
-
                     # TODO(will): finalize the interface. vLLM uses this to
                     # support torch dynamo compilation. They pass in
                     # attn_metadata, vllm_config, and num_tokens. We can pass in
